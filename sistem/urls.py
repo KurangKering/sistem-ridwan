2bb42e770cf3f0a2b6d3a12c541a28d05fad038f
KurangKering <cybercature@gmail.com>
2021-07-12 17:30:03 +0700

    modified index page

--- sistem/urls.py
+++ sistem/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-
+from fcm import views as fcm_views
 urlpatterns = [
-	path('', views.index, name="index"),
+	path('', fcm_views.index_pengujian_kiper, name="index"),
     path('admin/', admin.site.urls),
     path('', include('fcm.urls')),
 
